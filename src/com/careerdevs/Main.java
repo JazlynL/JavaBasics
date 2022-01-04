639ca4bb1a4ac2af6d89cedf29a6cfe293e182f5
“JazlynL” <lubenjazlyn35@gmail.com>
2022-01-03 20:53:32 -0500

    Full name

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -3,6 +3,12 @@ package com.careerdevs;
 public class Main {
 
     public static void main(String[] args) {
+
+
+
 	// write your code here
+        String  fName = "Jazlyn";
+        String  lName = "Luben";
+        System.out.println(fName.concat(lName));
     }
 }

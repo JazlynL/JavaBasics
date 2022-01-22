4c975d09ef607eb81aa9149314e9b11eb0638781
“JazlynL” <lubenjazlyn35@gmail.com>
2022-01-22 18:13:45 -0500

    "Java basics challenge 3/3"

---
+++ src/com/careerdevs/NameGenerator.java
@@ -0,0 +1,10 @@
+package com.careerdevs;
+
+public class NameGenerator {
+
+    public static void  generateFullName(){
+        String firstName = "Jazlyn";
+        String lastName = "Luben";
+        System.out.println(" Oh Your Whole Name " + firstName + " " + lastName);
+    }
+}

f40c0b9e8e4b1c47ce72d35c1311e709178953ee
“JazlynL” <lubenjazlyn35@gmail.com>
2022-01-22 17:47:07 -0500

    Say Hello challenge A and B in Java

---
+++ src/com/careerdevs/animalfactory/animalfactory.java
@@ -0,0 +1,25 @@
+//package com.careerdevs.animalfactory;
+//
+//public class AnimalFactory {
+//    public String species;
+//    public Short legs;
+//    public Boolean livesOnLand;
+//
+//
+//
+//    public Animal (String species , Short legs , Boolean livesOnLand){
+//        this.species = species;
+//        this.legs = legs;
+//        this.livesOnLand = livesOnLand;
+//
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "AnimalFactory{" +
+//                "species='" + species + '\'' +
+//                ", legs=" + legs +
+//                ", livesOnLand=" + livesOnLand +
+//                '}';
+//    }
+//}
--- src/com/careerdevs/sayhello/Main.java
+++ src/com/careerdevs/sayhello/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main( String [] args){
         whatIsYourName();
+        whatIsYourFullName();
 
     }
     public static void whatIsYourName(){
@@ -13,5 +14,14 @@ public class Main {
         String fName = scanner.next();
         System.out.println(fName);
         System.out.println("Hello Nice To Meet You  " + fName);
+    }
+    public static void whatIsYourFullName(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your first name?");
+        String fullName = scanner.nextLine();
+        System.out.println("What is your last name?");
+        String lastName = scanner.nextLine();
+        System.out.println("Ahh your whole name is  " + fullName + " " + lastName);
+
     }
 }

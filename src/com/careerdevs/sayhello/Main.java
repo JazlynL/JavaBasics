a6d0814203c65b1558411800c305626ba22e58c1
“JazlynL” <lubenjazlyn35@gmail.com>
2022-01-24 17:09:26 -0500

    What is your Name again different inputs.

--- src/com/careerdevs/sayhello/Main.java
+++ src/com/careerdevs/sayhello/Main.java
@@ -6,6 +6,8 @@ public class Main {
     public static void main( String [] args){
         whatIsYourName();
         whatIsYourFullName();
+        whatIsYourAge();
+        doYouLikeBananas();
 
     }
     public static void whatIsYourName(){
@@ -23,5 +25,21 @@ public class Main {
         String lastName = scanner.nextLine();
         System.out.println("Ahh your whole name is  " + fullName + " " + lastName);
 
+    }
+
+    //What was your Name again assignment
+
+    public static void whatIsYourAge(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is your age?");
+        int userInput1 = scan.nextInt();
+        System.out.println(" So your age is " + userInput1);
+    }
+
+    public static void doYouLikeBananas(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("is it true you like bananas");
+        boolean userInput2 = scan.nextBoolean();
+        System.out.println(" you answered  " + userInput2);
     }
 }

4a0ce26a6243baf4f0f0c2ba667e675cf9d0d112
“JazlynL” <lubenjazlyn35@gmail.com>
2022-01-24 19:35:47 -0500

    "Car in Java"

--- src/com/careerdevs/carfactory/Car.java
+++ src/com/careerdevs/carfactory/Car.java
@@ -1,4 +1,16 @@
 package com.careerdevs.carfactory;
 
 public class Car {
+
+    public String model;
+    public String make;
+    public int mileage;
+    public int gasPercentage;
+
+    public Car(String model,String make,int mileage, int gasPercentage){
+        this.model = model;
+        this.make = make;
+        this.mileage = mileage;
+        this.gasPercentage= gasPercentage;
+    }
 }

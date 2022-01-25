9583afe07e7735f48a2d3cc545d44a3c4ba558ad
“JazlynL” <lubenjazlyn35@gmail.com>
2022-01-24 19:37:08 -0500

    "Car in Java"

--- src/com/careerdevs/carfactory/Main.java
+++ src/com/careerdevs/carfactory/Main.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String [] args){
-        createACar();
+//        createACar();
+        createACarObject();
 
     }
     public static void createACar(){
@@ -20,7 +21,17 @@ public class Main {
         System.out.println(owner + " owns the car "+ make + " " + model);
     }
     public  static  void createACarObject(){
-
+       Scanner carInfo = new Scanner(System.in);
+        System.out.println("What is your cars make?");
+       String make = carInfo.nextLine();
+        System.out.println("What is your cars model");
+        String model = carInfo.nextLine();
+        System.out.println("What is your cars Mileage");
+        int mileage = carInfo.nextInt();
+        System.out.println("What is the car percentage?");
+        int gasPercentage = carInfo.nextInt();
+//        Car car1 = new Car(make,model,mileage,gasPercentage);
+        System.out.println("This is the car info "+ make +" "+ model + " , This is the car mileage " + mileage + " this is the gas percentage " + gasPercentage );
 
     }
 
